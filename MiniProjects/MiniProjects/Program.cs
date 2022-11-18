d573c17575d4552f3ec38cfba922a7152ddb6836
Tori <toricook@berkeley.edu>
2022-11-18 14:35:34 -0300

    Add files via upload
    first commit, secret santa

---
+++ MiniProjects/MiniProjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using EASendMail;
+using Google.Apis.Auth;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiniProjects
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Contains("santa"))
+            {
+                await SecretSanta.Run();
+            }
+        }
+    }
+
+}
